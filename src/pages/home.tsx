754800a87a5be76eb19b2c7cb8e1616a5fe87823
meluiz <me.luizfelipe@gmail.com>
2022-07-31 21:28:20 -0300

    feat: base structure of homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,20 +1,67 @@
 import React from 'react'
 
-import { List, ListItem } from '@chakra-ui/react'
+import { Box, Flex, Heading } from '@chakra-ui/react'
+import CreateUserButton from '@component/containers/home/create-user'
 
 const Component: React.FC = function () {
   return (
-    <List>
-      <ListItem>
-        Chrome: <span id="chrome-version"></span>
-      </ListItem>
-      <ListItem>
-        Node: <span id="node-version"></span>
-      </ListItem>
-      <ListItem>
-        Electron: <span id="electron-version"></span>
-      </ListItem>
-    </List>
+    <Box w="100%" h="100vh" display="block" pos="relative">
+      <Box
+        w="100%"
+        h="80px"
+        display="block"
+        pos="relative"
+        bg="transparent"
+        zIndex={2}
+      >
+        <Flex
+          w="100%"
+          maxW="80%"
+          h="100%"
+          align="center"
+          justify="center"
+          gap="32px"
+          mx="auto"
+          textAlign="center"
+        >
+          <Flex w="100%" align="center" justify="flex-start" pos="relative">
+            <Heading
+              textTransform="lowercase"
+              color="zinc.900"
+              fontSize="32px"
+              fontWeight="bold"
+              userSelect="none"
+            >
+              intrack
+            </Heading>
+          </Flex>
+        </Flex>
+      </Box>
+      <Box
+        w="100%"
+        h="100%"
+        pos="absolute"
+        top="0"
+        right="0"
+        bottom="0"
+        left="0"
+        overflow="hidden"
+        bg="zinc.100"
+      >
+        <Flex
+          w="100%"
+          maxW="80%"
+          h="100%"
+          align="center"
+          justify="center"
+          gap="32px"
+          mx="auto"
+          textAlign="center"
+        >
+          <CreateUserButton size={128} />
+        </Flex>
+      </Box>
+    </Box>
   )
 }
 
