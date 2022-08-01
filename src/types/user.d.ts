b76df118d51635a4ed785bae0efd2be32592238e
meluiz <me.luizfelipe@gmail.com>
2022-07-31 21:29:11 -0300

    declaring user as Global

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,9 +1,11 @@
 import type { AvatarFullConfig } from 'react-nice-avatar'
 
-declare interface User {
-  _id: string
-  name: string
-  avatar: AvatarFullConfig
-  created_at: Date
-  updated_at: Date
+declare global {
+  interface User {
+    _id: string
+    name: string
+    avatar: AvatarFullConfig
+    created_at: Date
+    updated_at: Date
+  }
 }
